2d777f48c641707fcf90102d1f1da983fcf4fe39
EvertonOliveiraFernandes <everton@umov.me>
2022-03-11 15:50:23 -0300

    Refactor SRPMain

--- src/main/java/solid/principles/srp/SRPMain.java
+++ src/main/java/solid/principles/srp/SRPMain.java
@@ -1,10 +1,10 @@
 package solid.principles.srp;
 
-import solid.principles.srp.bad.RobotDoesEverything;
+import solid.principles.srp.bad.RobotDoesEverythingBad;
 
 public class SRPMain {
     public static void main(String[] args) {
-        RobotDoesEverything robotDoesEverything = new RobotDoesEverything("Bob");
-        robotDoesEverything.robotPresentation();
+        RobotDoesEverythingBad robotDoesEverythingBad = new RobotDoesEverythingBad("Bob", "cut");
+        robotDoesEverythingBad.robotPresentation();
     }
 }
