2571e5b0ddd5826e972baa7f5633a341fa893fa7
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2022-06-08 14:32:33 -0300

    Implements unit test of dependency inversion principle

--- src/test/java/principles/dip/good/CutterRobotTest.java
+++ src/test/java/principles/dip/good/CutterRobotTest.java
@@ -1,24 +1,32 @@
 package principles.dip.good;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import solid.principles.dip.good.CutterArm;
 import solid.principles.dip.good.CutterRobot;
+
+import static org.mockito.Mockito.times;
 
 class CutterRobotTest {
 
     @Mock
     private CutterArm cutterArm;
 
+    private CutterRobot cutterRobot;
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
+        cutterRobot = new CutterRobot(cutterArm);
     }
 
     @Test
-    void shoudCutSlice(){
-        CutterRobot cutterRobot = new CutterRobot(cutterArm);
+    void shouldCutSlice(){
         cutterRobot.cutSlice();
+        Mockito.verify(cutterArm, times(1)).cut();
     }
+
 }
