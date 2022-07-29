675eb59e8a23e424d7028383b9e4a51fd9de7754
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2022-07-28 21:49:06 -0300

    adds presentation in project readme.md file

--- src/main/java/solid/principles/lsp/good/ChildRobot.java
+++ src/main/java/solid/principles/lsp/good/ChildRobot.java
@@ -2,7 +2,7 @@ package solid.principles.lsp.good;
 
 import solid.principles.lsp.FatherRobot;
 
-public class ChildRobot extends FatherRobot implements MakeCappuccino {
+public class ChildRobot extends FatherRobot implements MakeCappuccino  {
     private final String name = "Eden";
 
     @Override
